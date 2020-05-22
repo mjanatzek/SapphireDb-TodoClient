5018b4000352f1546a85102d97d15799083b4427
Morris Janatzek <mjanatzek@brockhaus-ag.de>
2020-05-22 10:50:58 +0200

    updated readme

--- src/app/todo/add/add.component.ts
+++ src/app/todo/add/add.component.ts
@@ -1,12 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {SapphireDbService} from 'ng-sapphiredb';
-import {TodoItem} from '../../shared/models/todo-item';
-import {Router} from '@angular/router';
-
-import * as moment from 'moment';
-import {Observable} from 'rxjs';
-import {User} from '../../shared/models/user';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add',
@@ -15,46 +7,10 @@ import {User} from '../../shared/models/user';
 })
 export class AddComponent implements OnInit {
 
-  users$: Observable<User[]>;
+  constructor() {
 
-  form: FormGroup;
-
-  constructor(private db: SapphireDbService, private router: Router) {
-    this.form = new FormGroup({
-      title: new FormControl(undefined, [ Validators.required ]),
-      description: new FormControl(undefined),
-      assignedToId: new FormControl(undefined, [ Validators.required ]),
-      dueDate: new FormControl(moment(), [ Validators.required ]),
-    });
-
-    this.users$ = this.db.collection<User>('users').values();
   }
 
   ngOnInit(): void {
-  }
-
-  save() {
-    const formValue: TodoItem = this.form.value;
-    this.db.collection('todoItems')
-      .add(formValue)
-      .subscribe(result => {
-        if (result.results[0].error) {
-          alert('Error');
-          return;
-        }
-
-        const validationResults = result.results[0].validationResults;
-        if (validationResults) {
-          const message = Object.keys(validationResults)
-            .map(key => [key, validationResults[key]])
-            .map(([key, vr]: [string, string[]]) => `${key}: ${vr.join(',')}`)
-            .join(',\n');
-
-          alert(message);
-          return;
-        }
-
-        this.router.navigateByUrl('/todo/list');
-      });
   }
 }
--- src/app/todo/list/list.component.ts
+++ src/app/todo/list/list.component.ts
@@ -1,43 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {BehaviorSubject, Observable, Subject, Subscription} from 'rxjs';
-import {TodoItem} from '../../shared/models/todo-item';
-import {SapphireDbService} from 'ng-sapphiredb';
-import {UserService} from '../../shared/user.service';
-import {DefaultCollection} from 'sapphiredb';
-import {debounceTime} from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.less']
 })
-export class ListComponent implements OnInit, OnDestroy {
-  todoCollection: DefaultCollection<TodoItem>;
-  todoItems$: Observable<TodoItem[]>;
+export class ListComponent implements OnInit {
+  constructor() {
 
-  changeProgressSubject$ = new Subject<[TodoItem, number]>();
-
-  private changeProgressSubscription?: Subscription;
-
-  constructor(private db: SapphireDbService, public userService: UserService) {
-    this.todoCollection = this.db.collection<TodoItem>('todoItems');
-    this.todoItems$ = this.todoCollection.values();
-
-    this.changeProgressSubject$.pipe(
-      debounceTime(200)
-    ).subscribe(([changedTodo, newProgress]) => {
-      this.todoCollection.update({
-        ...changedTodo,
-        progress: newProgress
-      });
-    });
   }
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    this.changeProgressSubscription?.unsubscribe();
   }
 
 }
